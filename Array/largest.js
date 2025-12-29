3c4e3b19bb0af1f0226d428425e41172424093d1
Mohan-Badiger <mohanbadiger250@gmail.com>
2025-12-29 22:38:17 +0530

    map array method

--- Array/largest.js
+++ Array/largest.js
@@ -1,4 +1,4 @@
-function largest(arr) {
+/* function largest(arr) {
     let large = arr[0];
     for (let i = 0; i <= arr.length; i++) {
         if (arr[i] > large) {
@@ -9,3 +9,13 @@ function largest(arr) {
 }
 
 console.log(largest([4, 5, 1, 3]));
+*/
+
+
+let numbers = [1,2,3,4,5]
+
+let doubled = numbers.map((value)=>{
+    return value *2;
+})
+
+console.log(doubled)
