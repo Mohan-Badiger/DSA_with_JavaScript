1388fcc7b2249b13a723998daf0517b61d1df1fb
Mohan-Badiger <mohanbadiger250@gmail.com>
2025-12-30 23:54:40 +0530

    refactor: remove commented code and improve filter logic in largest.js

--- Array/largest.js
+++ Array/largest.js
@@ -26,3 +26,23 @@ let userNeed = 2
 let pay = petrolPrices*userNeed
 
 console.log(pay);*/
+
+
+let marks = [10,15,30,20,30,40,50];
+
+// let results = []
+
+// for(let i =0; i< marks.length; i++){
+//     if(marks[i]>20){
+//         results.push(marks[i])
+//     }
+// }
+
+// console.log(results);
+
+
+let result = marks.filter((value)=>{
+  return value>20 && [...new Set(value)]
+})
+
+console.log(result);
