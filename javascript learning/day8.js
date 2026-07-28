8a0e6ce19388e5a8b0de2f46531f4da85a75e585
Mohan-Badiger <mohanbadiger250@gmail.com>
2026-07-28 22:48:09 +0530

    feat: add various DSA practice scripts, utility modules, and supporting documentation

---
+++ javascript learning/day8.js
@@ -0,0 +1,8 @@
+// if array items are dublicate return true else return false
+
+// let dublicate = (arr) => {
+//     let set = new Set(arr);
+//     return set.size !== arr.length
+// }
+
+// console.log(dublicate([1, 2, 1, 3, 5, 6, 7]));
