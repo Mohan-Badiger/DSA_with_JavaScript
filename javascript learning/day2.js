16ef67a7c8bff453f65e370ed69b5347e11f93eb
Mohan-Badiger <mohanbadiger250@gmail.com>
2026-07-28 22:47:42 +0530

    feat: add various DSA practice scripts, utility modules, and supporting documentation

---
+++ javascript learning/day2.js
@@ -0,0 +1,76 @@
+// let a = Number(prompt("Enter Length : "))
+// let b = Number(prompt("Enter Breadth : "))
+
+// let area = a*b
+// let perimeter = 2*(a+b)
+
+// console.log("Area of Rectangle is : ",area);
+// console.log("Perimeter of Rectangle is : ",perimeter);
+
+// ------------------------------------------------------//
+// let otp = Math.floor((Math.random()*1000)+1000);
+// console.log(otp);
+
+
+// ------------------------------------------------------//
+//Area of triangle by Heron's formula
+// let a = 6;
+// let b = 6;
+// let c = 6;
+
+// let s = (a+b+c)/2;
+
+// let area = Math.sqrt(s*(s-a)*(s-b)*(s-c))
+
+// console.log(s);
+// console.log(area);
+
+// ------------------------------------------------------//
+//Area and Circumference of circle 
+// let r = 3;
+ 
+// let area = 3.142*r*r; //PI*R*R
+// let Circumference = 2*3.142*r;
+
+// console.log(area);
+// console.log(Circumference);
+
+
+//-------------------------------------------------------//
+// sum of n natural numbers
+// let n = 5;
+// let sum = 0;
+
+// for(let i=1; i<=n; i++){
+//     sum = sum + i;
+// }
+// console.log(sum)
+
+//-------------------------------------------------------//  
+// function gcd(m,n){
+//         if(n == 0){
+//                 return m;
+//             }else if(m<n){
+//                 return gcd(n,m)
+//             }else{
+//                 return gcd(n, m%n)
+//         }
+//     }
+                
+// console.log(gcd(5,23))
+                
+//-------------------------------------------------------//  
+
+// function dev(a, b){
+//     let result = []
+//     if(b>0){
+//         let c = a/b;
+//         let flr = Math.floor(Math.abs(c));
+//         let ceil = Math.ceil(Math.abs(c));
+//         result.push(flr, ceil)
+//         console.log(result);
+//     }
+    
+// }
+
+// dev(5,5)
