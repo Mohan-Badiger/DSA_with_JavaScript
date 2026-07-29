5811572d38afa9d2cf77441830fab34cd9b4d23e
Mohan-Badiger <mohanbadiger250@gmail.com>
2026-07-29 22:41:58 +0530

    feat: add DSA implementations for array search, odd-even counting, and min-max finding

---
+++ Array/arraySearch.js
@@ -0,0 +1,15 @@
+//Array Search
+// Given an array, arr[] of n integers, and an integer element x, find whether element x is present in the array. Return the index of the first occurrence of x in the array, or -1 if it doesn't exist.
+
+const arrySearch = (arr, x) => {
+    let arrHas = arr.includes(x)
+    // let index = arr.indexOf(x)
+    if (arrHas) {
+        return arr.indexOf(x)
+    } else {
+        return -1
+    }
+
+}
+
+console.log(arrySearch([10, 20, 30, 40, 50], 30));
