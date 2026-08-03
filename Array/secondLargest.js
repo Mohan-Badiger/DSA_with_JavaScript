f7de2aed7f78defc886db6b6179db80b4b1b2245
Mohan-Badiger <mohanbadiger250@gmail.com>
2026-08-03 22:19:18 +0530

    feat: secondLargest

---
+++ Array/secondLargest.js
@@ -0,0 +1,14 @@
+// function secondLargest(arr) {
+//     let largest = arr[0];
+//     let secondLargest = arr[1];
+
+//     for (let i = 2; i < arr.length; i++) {
+//         if (arr[i] > largest) {
+//             secondLargest = largest
+//             largest = arr[i]
+//         }
+//     }
+//     return secondLargest
+// }
+
+// console.log(secondLargest([6, 1, 2, 4, 8, 7]));
