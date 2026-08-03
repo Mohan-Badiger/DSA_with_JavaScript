59b5f1bc13441bd89e780c13b1fab106bcd5a618
Mohan-Badiger <mohanbadiger250@gmail.com>
2026-08-03 22:19:27 +0530

    feat:missing array

---
+++ Array/missingArray.js
@@ -0,0 +1,12 @@
+const missingNum = (arr) => {
+    let sum = 0;
+    let n = arr.length + 1;
+    let sumOfN = n * (n + 1) / 2;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sumOfN - sum;
+
+}
+
+console.log(missingNum([1, 2, 3, 4, 6]));
